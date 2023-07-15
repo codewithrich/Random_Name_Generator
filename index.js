d30f570915150dd993f8e30811a816884aac1d3c
Richardson <104580727+codewithrich@users.noreply.github.com>
2023-07-15 14:24:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const sillyname = require('sillyname');
+const generate = sillyname();
+
+let richiee= () => {
+console.log("Welcome to Richiee's Random Name Generator")
+console.log (`Hey Try this "${generate}"`)
+}
+
+richiee()
